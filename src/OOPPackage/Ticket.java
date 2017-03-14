8bea5bd359906cd79cb282966361fe97c6e06505
WaffleChampion <bendik1234@hotmail.no>
2017-03-14 12:48:12 +0100

    implemented ticket

--- src/OOPPackage/Ticket.java
+++ src/OOPPackage/Ticket.java
@@ -2,19 +2,34 @@ package OOPPackage;
 
 /**
  *
- * @author Thomas Sund Mjåland
+ * @author Bendik Matheussen Delp
  */
 public class Ticket {
     int ticketID;
-    Seat seat;
+    int seatID;
     Passenger passenger;
     int price;
     String flightID;
     
+    public Ticket(int seatId, int _price, String flight, Passenger _passenger)
+    {
+        seatID = seatId;
+        price = _price;
+        flightID = flight;
+        passenger = _passenger;
+    }
     public int getID(){
         return ticketID;
     }
     public void setID(int _ID){
         ticketID=_ID;
+    }
+    public int getPrice()
+    {
+        return price;
+    }
+    public String getFlightID()
+    {
+       return flightID; 
     }
 }
